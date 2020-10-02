3ea41f04104e48fe84f122a0babe44af94f1437b
Otavio Pace <otaviopp8@gmail.com>
2020-10-01 21:15:28 -0300

    refactor: extract _physics_process new move method

--- src/player.rs
+++ src/player.rs
@@ -23,18 +23,32 @@ impl Player {
 
         let godot_singleton = Input::godot_singleton();
 
+        let right_strength = godot_singleton.get_action_strength(ui_right);
+        let left_strength = godot_singleton.get_action_strength(ui_left);
+        let down_strength = godot_singleton.get_action_strength(ui_down);
+        let up_strength = godot_singleton.get_action_strength(ui_up);
+
+        self.r#move(right_strength, left_strength, down_strength, up_strength);
+
+        owner.move_and_collide(self.velocity, false, false, false);
+    }
+
+    fn r#move(
+        &mut self,
+        right_strength: f64,
+        left_strength: f64,
+        down_strength: f64,
+        up_strength: f64,
+    ) {
         let mut input_vector = Vector2::new(0.0, 0.0);
-        input_vector.x = (godot_singleton.get_action_strength(ui_right)
-            - godot_singleton.get_action_strength(ui_left)) as f32;
-        input_vector.y = (godot_singleton.get_action_strength(ui_down)
-            - godot_singleton.get_action_strength(ui_up)) as f32;
+
+        input_vector.x = (right_strength - left_strength) as f32;
+        input_vector.y = (down_strength - up_strength) as f32;
 
         if input_vector != Vector2::new(0.0, 0.0) {
             self.velocity = input_vector;
         } else {
             self.velocity = Vector2::new(0.0, 0.0);
         }
-
-        owner.move_and_collide(self.velocity, false, false, false);
     }
 }
