6dda5f68e6acac43a53ebcc372550923fe3db8ee
Otavio Pace <otaviopp8@gmail.com>
2020-10-04 00:02:22 -0300

    player: ajust movement to use move_towards

--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,8 @@
 use gdnative::prelude::{Input, KinematicBody2D, NativeClass, Vector2, Vector2Godot};
 
-const ACCELERATION: f64 = 10.0;
-const MAX_SPEED: f64 = 100.0;
-const FRICTION: f64 = 10.0;
+const ACCELERATION: f64 = 500.0;
+const MAX_SPEED: f64 = 80.0;
+const FRICTION: f64 = 500.0;
 
 #[derive(NativeClass)]
 #[inherit(KinematicBody2D)]
@@ -18,7 +18,7 @@ impl Player {
     }
 
     #[export]
-    fn _physics_process(&mut self, owner: &KinematicBody2D, delta: f64) {
+    fn _physics_process(&mut self, owner: &KinematicBody2D, delta: f32) {
         let godot_singleton = Input::godot_singleton();
 
         let right_strength = godot_singleton.get_action_strength("ui_right");
@@ -34,7 +34,7 @@ impl Player {
             delta,
         );
 
-        owner.move_and_collide(self.velocity, true, true, false);
+        owner.move_and_collide(self.velocity * delta, true, true, false);
     }
 
     fn r#move(
@@ -43,7 +43,7 @@ impl Player {
         left_strength: f64,
         down_strength: f64,
         up_strength: f64,
-        delta: f64,
+        delta: f32,
     ) {
         let mut input_vector = Vector2::new(0.0, 0.0);
 
@@ -52,14 +52,13 @@ impl Player {
 
         if input_vector != Vector2::new(0.0, 0.0) {
             input_vector = input_vector.normalize();
-            self.velocity += input_vector * ACCELERATION as f32 * delta as f32;
             self.velocity = self
                 .velocity
-                .clamp_length(0.0, MAX_SPEED as f32 * delta as f32);
+                .move_towards(input_vector * MAX_SPEED as f32, ACCELERATION as f32 * delta);
         } else {
             self.velocity = self
                 .velocity
-                .move_towards(Vector2::new(0.0, 0.0), (FRICTION * delta) as f32);
+                .move_towards(Vector2::new(0.0, 0.0), FRICTION as f32 * delta);
         }
     }
 }
